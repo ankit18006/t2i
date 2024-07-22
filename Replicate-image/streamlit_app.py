f4c1b13505023ff21d7b5ec63681e23d4855e527
Ankit Shukla <163987630+ankit18006@users.noreply.github.com>
2024-07-22 10:36:31 +0530

    Update streamlit_app.py

--- Replicate-image/streamlit_app.py
+++ Replicate-image/streamlit_app.py
@@ -7,20 +7,17 @@ from utils import icon
 from streamlit_image_select import image_select
 
 # UI configurations
-st.set_page_config(page_title="Replicate Image Generator",
+st.set_page_config(page_title="Image Generator",
                    page_icon=":bridge_at_night:",
                    layout="wide")
 icon.show_icon(":foggy:")
-st.markdown("# :rainbow[Text-to-Image Artistry Studio]")
+st.markdown("# :rainbow[Text 2 Image Studio]")
 
 # API Tokens and endpoints from `.streamlit/secrets.toml` file
 REPLICATE_API_TOKEN = st.secrets["REPLICATE_API_TOKEN"]
 REPLICATE_MODEL_ENDPOINTSTABILITY = st.secrets["REPLICATE_MODEL_ENDPOINTSTABILITY"]
 
-# Resources text, link, and logo
-replicate_text = "Stability AI SDXL Model on Replicate"
-replicate_link = "https://replicate.com/stability-ai/sdxl"
-replicate_logo = "https://storage.googleapis.com/llama2_release/Screen%20Shot%202023-07-21%20at%2012.34.05%20PM.png"
+
 
 # Placeholders for images and gallery
 generated_images_placeholder = st.empty()
@@ -36,8 +33,7 @@ def configure_sidebar() -> None:
     """
     with st.sidebar:
         with st.form("my_form"):
-            st.info("**Yo fam! Start here ↓**", icon="👋🏾")
-            with st.expander(":rainbow[**Refine your output here**]"):
+            with st.expander(":rainbow[**Customize your output here**]"):
                 # Advanced Settings (for the curious minds!)
                 width = st.number_input("Width of output image", value=1024)
                 height = st.number_input("Height of output image", value=1024)
@@ -56,9 +52,9 @@ def configure_sidebar() -> None:
                 high_noise_frac = st.slider(
                     "Fraction of noise to use for `expert_ensemble_refiner`", value=0.8, max_value=1.0, step=0.1)
             prompt = st.text_area(
-                ":orange[**Enter prompt: start typing, Shakespeare ✍🏾**]",
-                value="An astronaut riding a rainbow unicorn, cinematic, dramatic")
-            negative_prompt = st.text_area(":orange[**Party poopers you don't want in image? 🙅🏽‍♂️**]",
+                ":orange[**Enter prompt: **]",
+                value="A serene forest clearing with a mystical, glowing portal in the center")
+            negative_prompt = st.text_area(":orange[**Something you don't want in image? 🙅🏽‍♂️**]",
                                            value="the absolute worst quality, distorted features",
                                            help="This is a negative prompt, basically type what you don't want to see in the generated image")
 
@@ -66,23 +62,29 @@ def configure_sidebar() -> None:
             submitted = st.form_submit_button(
                 "Submit", type="primary", use_container_width=True)
 
-        # Credits and resources
-        st.divider()
-        st.markdown(
-            ":orange[**Resources:**]  \n"
-            f"<img src='{replicate_logo}' style='height: 1em'> [{replicate_text}]({replicate_link})",
-            unsafe_allow_html=True
-        )
+       
         st.markdown(
             """
             ---
-            Follow me on:
+            Follow me on :
 
-            𝕏 → [@tonykipkemboi](https://twitter.com/tonykipkemboi)
+            Instagram → [Just Mahesh](https://www.instagram.com/just_mahesh_75)
 
-            LinkedIn → [Tony Kipkemboi](https://www.linkedin.com/in/tonykipkemboi)
+            Listen Here :
+
+            Music →   [Music Here](https://just-mahesh1.web.app)
 
             """
+        )
+
+
+
+
+       # Credits and resources
+        st.divider()
+        st.markdown(
+            ":orange[**© 2024 Mahesh U**]  \n"
+            
         )
 
         return submitted, width, height, num_outputs, scheduler, num_inference_steps, guidance_scale, prompt_strength, refine, high_noise_frac, prompt, negative_prompt
@@ -109,9 +111,9 @@ def main_page(submitted: bool, width: int, height: int, num_outputs: int,
         negative_prompt (str): Text prompt for elements to avoid in the image.
     """
     if submitted:
-        with st.status('👩🏾‍🍳 Whipping up your words into art...', expanded=True) as status:
-            st.write("⚙️ Model initiated")
-            st.write("🙆‍♀️ Stand up and strecth in the meantime")
+        with st.status('Loading up the awesomeness, please wait a sec...', expanded=True) as status:
+            st.write("Please hold on, magic is happening!")
+            st.write("The wait will be worth it, almost done!")
             try:
                 # Only call the API if the "Submit" button was pressed
                 if submitted:
@@ -135,7 +137,7 @@ def main_page(submitted: bool, width: int, height: int, num_outputs: int,
                         )
                         if output:
                             st.toast(
-                                'Your image has been generated!', icon='😍')
+                                'Your image has been generated!')
                             # Save generated image to session state
                             st.session_state.generated_image = output
 
@@ -175,29 +177,6 @@ def main_page(submitted: bool, width: int, height: int, num_outputs: int,
                 print(e)
                 st.error(f'Encountered an error: {e}', icon="🚨")
 
-    # If not submitted, chill here 🍹
-    else:
-        pass
-
-    # Gallery display for inspo
-    with gallery_placeholder.container():
-        img = image_select(
-            label="Like what you see? Right-click and save! It's not stealing if we're sharing! 😉",
-            images=[
-                "gallery/farmer_sunset.png", "gallery/astro_on_unicorn.png",
-                "gallery/friends.png", "gallery/wizard.png", "gallery/puppy.png",
-                "gallery/cheetah.png", "gallery/viking.png",
-            ],
-            captions=["A farmer tilling a farm with a tractor during sunset, cinematic, dramatic",
-                      "An astronaut riding a rainbow unicorn, cinematic, dramatic",
-                      "A group of friends laughing and dancing at a music festival, joyful atmosphere, 35mm film photography",
-                      "A wizard casting a spell, intense magical energy glowing from his hands, extremely detailed fantasy illustration",
-                      "A cute puppy playing in a field of flowers, shallow depth of field, Canon photography",
-                      "A cheetah mother nurses her cubs in the tall grass of the Serengeti. The early morning sun beams down through the grass. National Geographic photography by Frans Lanting",
-                      "A close-up portrait of a bearded viking warrior in a horned helmet. He stares intensely into the distance while holding a battle axe. Dramatic mood lighting, digital oil painting",
-                      ],
-            use_container_width=True
-        )
 
 
 def main():
